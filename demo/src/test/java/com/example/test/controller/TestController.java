09d264332522ed6144a97f319b30bf232835a4ef
choi seongyun <sonunch@hanmail.net>
2021-12-26 20:56:56 +0900

    spring setting

---
+++ demo/src/test/java/com/example/test/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.example.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController{
+    @RequestMapping(value = "/home") 
+    public String home(){ 
+        return "index.html"; 
+    }
+
+
+}
