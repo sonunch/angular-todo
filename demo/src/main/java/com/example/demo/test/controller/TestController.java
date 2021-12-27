a62619aa6af688415c8906a1c43c3b1bde05573a
choi seongyun <sonunch@hanmail.net>
2021-12-28 00:34:48 +0900

    false

--- demo/src/main/java/com/example/demo/test/controller/TestController.java
+++ demo/src/main/java/com/example/demo/test/controller/TestController.java
@@ -1,14 +1,28 @@
 package com.example.demo.test.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class TestController{
     @RequestMapping(value = "/home") 
     public String home(){ 
-        return "index.html"; 
+        return "test"; 
     }
-
+    @RequestMapping("/test") 
+    public ModelAndView test() throws Exception{ 
+        ModelAndView mav = new ModelAndView("test"); 
+        mav.addObject("name", "csy"); 
+        List<String> testList = new ArrayList<String>(); 
+        testList.add("choi"); 
+        testList.add("seong"); 
+        testList.add("yun"); 
+        mav.addObject("list", testList); 
+        return mav; 
+    }
 
 }
--- demo/src/main/java/com/example/demo/test/controller/TestRestController.java
+++ demo/src/main/java/com/example/demo/test/controller/TestRestController.java
@@ -1,16 +1,23 @@
 package com.example.demo.test.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
 
 @RestController
 public class TestRestController {
-
+    
     @RequestMapping(value="testValue", method = RequestMethod.GET)
     public String getTestValue(){
         String TestValue = "레스트 컨트롤러 잘 가니?";
         return TestValue;
     }
     
+
 }
